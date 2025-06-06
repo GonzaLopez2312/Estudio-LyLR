299af44dd6360cc5ff17b8c193253b2f0856e8be
GonzaLopez2312 <23gonzalopez@gmail.com>
2025-06-05 21:53:20 -0300

    Avances Sección Presentación, Correcciones en Header

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,17 @@ import './App.css';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Body from './components/Body';
+import Presentation from './components/Presentation';
 
 function App() {
   return (
     <>
       <Header />
+      <Presentation />
       <Body />
       <Footer />
     </>
   );
 }
 
-export default App
+export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,20 +1,23 @@
-import './Header.css';
-
+import "./Header.css";
 
 function Header() {
-    return(
-        <header className='header'>
-        <div className='logoLyLR-header'>
-            <img src= "src/assets/images/logoLyLR.png" alt='LyLR' className='logoLyLR-header'/>
-        </div>
-        <nav className='nav-sections'>
-            <a href='#inicio'>Inicio</a>
-            <a href='#nosotros'>Nosotros</a>
-            <a href='#servicios'>Servicios</a>
-            <a href='#contacto'>Contacto</a>
-        </nav>
-        </header>
-    );
+  return (
+    <header className="header">
+      <div className="logo-LyLR-header">
+        <img
+          src="src/assets/images/logoLyLR.png"
+          alt="LyLR"
+          className="logoLyLR-header"
+        />
+      </div>
+      <nav className="nav-sections">
+        <a href="#inicio">Inicio</a>
+        <a href="#nosotros">Nosotros</a>
+        <a href="#servicios">Servicios</a>
+        <a href="#contacto">Contacto</a>
+      </nav>
+    </header>
+  );
 }
 
 export default Header;
